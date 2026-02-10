c49dcf8494a9d8bd343671b5d10c687cd6ebcfc3
Ajani Henry <henryajani.ah@gmail.com>
2026-02-10 14:12:08 -0500

    Completed hardware section.

--- backend/app/mqtt.py
+++ backend/app/mqtt.py
@@ -16,7 +16,7 @@ class MQTT:
     ID = f"IOT_B_{randint(1,1000000)}"
 
     #  1. DEFINE ALL TOPICS TO SUBSCRIBE TO. BELOW ARE SOME EXAMPLES. YOUR ARE REQUIRED TO CHANGE THESE TO TOPICS THAT FITS YOUR USE CASE
-    sub_topics = [("620012345_pub", 0), ("620012345", 0), ("620012345_sub", 0)] #  A list of tuples of (topic, qos). Both topic and qos must be present in the tuple.
+    sub_topics = [("620172829_pub", 0), ("620172829", 0), ("620172829_sub", 0)] #  A list of tuples of (topic, qos). Both topic and qos must be present in the tuple.
 
 
     def __init__(self,mongo):
